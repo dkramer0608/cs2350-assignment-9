736a8975ec4c95c8a9f9dcd14a67a1069a189f99
Danielle <daniellekramer@mail.weber.edu>
2022-04-08 10:08:53 -0600

    final

---
+++ src/javascripts/books.js
@@ -0,0 +1,95 @@
+export const books = [{
+    "id": 1,
+    "title": "And Then There Were None",
+    "author": "Agatha Christie",
+    "released": "November 6 1939",
+    "plot": "The story follows ten people who are brought together, for various reasons, to an empty mansion on an island. The mysterious hosts of this strange party are not present, but left instructions for two of the ten to tend the house as the housekeeper and cook. As the days unfold in accordance with the lyrics of a nursery rhyme, each invitee is forced to face the music (literally) and bear the consequences of their troubling pasts, as death will come for them one by one.",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/51MlxNgCsyL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    "id": 2,
+    "title": "The Big Sleep",
+    "author": "Raymond Chandler",
+    "released": "1939",
+    "plot": "As such, The Big Sleep is no ordinary story: private eye Philip Marlowe gets hired to investigate the blackmailing of Carmen Sternwood, the second daughter of a wealthy general. The further he digs into this messy business, the more complicated the story gets, as Carmen continues to be blackmailed by others in a web of unexpected relations between the characters.",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/41KAQ5OZoGL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+
+  {
+    "id": 3,
+    "title": "The Da Vinci Code",
+    "author": "Dan Brown",
+    "released": "March 18, 2003",
+    "plot": "In this volume, Professor Robert Langdon is brought to Paris on a whirl to shed some light on a bizarre murder in the Louvre. As he and sidekick cryptologist Neveu tries to decode the artistic riddles left at the scene, all of which are related to the works of Leonardo da Vinci, Dan Brown takes readers scrambling through the City of Love, speechless.",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/41KuPr7kpZL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+
+  {
+    "id": 4,
+    "title": "The Girl with the Dragon Tattoo",
+    "author": "Stieg Larsson",
+    "released": "August 2005",
+    "plot": "The first book of Stieg Larssons Millennium series introduces us to journalist Mikael Blomkvist and freelance hacker Lisbeth Salander. Following two separate strings of events, the characters eventually find themselves both trying to find the person who, forty years ago, supposedly killed Harriet Vanger — niece of one of the wealthiest men in Sweden. Blomkvist is invited to stay over at the wealthy familys island, where he comes into contact with other family members who were present at the scene years ago, and begins to wonder if any of them were involved.",
+    "poster": "https://m.media-amazon.com/images/P/0307949486.01._SCLZZZZZZZ_SX500_.jpg",
+  },
+
+  {
+    "id": 5,
+    "title": "The Day of the Jackal",
+    "author": "Frederick Forsyth",
+    "released": "1967",
+    "plot": "“The Jackal” is the codename of the assassin hired to kill French President Charles de Gaulle in this enthralling tale. Whats more thrilling is the fact that this kill order came from within the government and thus must be covered up well. The Jackals challenge is thus two-fold — to circumvent the heavy safeguarding reserved for one of the most important men on Earth, and to protect his own identity, even from his employers.",
+    "poster": "https://m.media-amazon.com/images/P/0451239377.01._SCLZZZZZZZ_SX500_.jpg",
+  },
+
+
+  {
+    "id": 6,
+    "title": "Big Little Lies",
+    "author": "Liane Moriarty",
+    "released": "July 2014",
+    "plot": "Single mother Jane sends her son to kindergarten and befriends two mothers — Madeleine and Celeste. Along with their friendships is an array of family dramas, from ex- or abusive husbands to dark pasts. Jane doesnt know it, but theres a piece of her past that makes her fit perfectly into this wild puzzle. No one ever displays their domestic problems in their totality to others, not even to friends, and that makes Big Little Lies so much more captivating. ",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/41KAQ5OZoGL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+
+
+  {
+    "id": 7,
+    "title": "The Girl on the Train",
+    "author": "Paula Hawkins",
+    "released": "January 6, 2015",
+    "plot": "Rachel Watson takes a train from her home into the city center everyday, and to kill the time, she often spends much of it looking at the same houses that pass by her. She makes up stories for the lives she observes, stories that are better than her own, free of divorce and alcoholism. One day, she witnesses something that turns Rachel from a mere observer of the lives of this particular street to an active participant in it.",
+    "poster": "https://m.media-amazon.com/images/P/1594634025.01._SCLZZZZZZZ_SX500_.jpg",
+  },
+
+
+  {
+    "id": 8,
+    "title": "The Deep Blue Good",
+    "author": "John D. MacDonald",
+    "released": "May 21, 1964",
+    "plot": "As we are introduced to this tall, charming, and righteous character, he is swept away on a mission to find military man Junior Allen, a serial rapist and murderer. Allen has also discovered a smuggled treasure buried somewhere in Florida, and is using that to fund his malicious exploits. The difficult responsibility of trying to locate this psychopath falls onto McGees shoulders, the only person with the methodological patience to pick up Allens trace. Too often, the protagonist of detective stories are portrayed as being rational to the point of cold-hearted; its probably worth your while to change it up a little with Travis McGees quest for goodness.",
+    "poster": "https://m.media-amazon.com/images/P/0812983920.01._SCLZZZZZZZ_SX500_.jpg",
+  },
+
+
+  {
+    "id": 9,
+    "title": "The Sweetness at the Bottom of the Pie",
+    "author": "Alan Bradley",
+    "released": "January 22, 2009",
+    "plot": "Not all of the best mystery books have to leave a heavy sense of dread at the bottom of your stomach, and The Sweetness at the Bottom of the Pie is proof of that. The sleuth who saves the day in this novel is Flavia, an intuitive 11-year-old whose father is accused of murder. A stranger has ended up dead in the familys yard, one who happened to have been seen arguing with Flavias dad days before. Determined that her stamp-loving father, who has been heart-broken since the death of his wife, would never kill anyone, Flavia tours the town to try and prove his innocence. Light-hearted as it may sound, this novels puzzle is incredibly well-crafted and its classical mystery style, reminiscent of the works of Agatha Christie and Josephine Tey, makes it easy to finish the volume in one sitting. ",
+    "poster": "https://m.media-amazon.com/images/P/0385343493.01._SCLZZZZZZZ_SX500_.jpg",
+  },
+
+
+  {
+    "id": 10,
+    "title": "The Silence of the Lambs",
+    "author": "Thomas Harris",
+    "released": "May 19, 1988",
+    "plot": "FBI agent Clarice Stirling investigates a serial killer, “Buffalo Bill,” who preys on young women, and who potentially is linked to psychiatrist and cannibalistic murderer Hannibal Lecter. In order to weed a clue out from Lecter about Bills whereabouts, Stirling visits the psych ward where Lecter is imprisoned. However, her shuddering exchanges seem to reveal less about the killer on the loose, and more about Lecters astounding ability to get into the head of his victims.",
+    "poster": "https://images-na.ssl-images-amazon.com/images/I/41B4fijwh8L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+
+]
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,3 +5,61 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 //TODO - Your ES6 JavaScript code (if any) goes here
 import 'bootstrap'
+
+import { books } from './books'
+
+for(let b of books){
+
+    let b_thumb = document.getElementById('b' + m.id)
+    m_thumb.innerHTML = `
+        <img src="${b.poster}" alt="${b.title}" class="img-thumbnail"/>
+    `
+
+        b_thumb.onclick = function() {
+            displayBooks(b)
+        }
+
+}
+
+let featured_books = document.querySelector(".featured")
+function displayBooks(books) {
+    featured_movie.innerHTML = `
+    <div class="card">
+          <div class="card-header">${movie.title}</div>
+          <img src="${movie.poster}" class="card-img-top" alt="${movie.title}">
+          <div class="card-body">
+            <h5 class="card-title"><small>${movie.year}, ${movie.genre}</small></h5>
+            <p class="card-text">${movie.plot}</p>
+          </div>
+          <div class="card-footer text-muted">
+            <div class="row row-cols-3">
+              <div class="col"><strong>Rating: </strong> ${books.author}</div>
+              <div class="col"><strong>Rated: </strong>${books.released}</div>
+              <div class="col"><strong>Voted: </strong>${books.plot}</div>
+          </div>
+        </div>
+      </div>
+    `
+}
+
+
+function searchBooks(event) {
+    event.preventDefault()
+
+    let input = document.querySelector('[type="search"]').value ||""
+    let count = 0
+    for(let b of books) {
+        if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1) {
+         document.querySelector(`#m${m.id}`).classList.add('d-none')
+    }else {
+        document.querySelector(`#m${m.id}`).classList.remove('d-none')
+        count++
+    }
+}
+ featured_books.innerHTML = count ==0 ? 'Nothing was found' : ''
+
+}
+
+document.querySelector("button").onclick = searchBooks
+document.querySelector('[type="search"]').onsearch = searchBooks
+document.querySelector("form").onsubmit = searchBooks
